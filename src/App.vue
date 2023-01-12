9b265f79687947bc5937c2a518f6076ec4974873
smartchaoqi <qcq11111111@163.com>
2023-01-12 13:23:59 +0800

    移动端rem适配

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,13 @@
   <div id="app">
     <p>黑马头条</p>
     <i class="toutiao toutiao-shipin"></i>
+    <van-button type="primary">主要按钮</van-button>
   </div>
 </template>
 
 <script>
 export default {
-  name:"App",
+  name:'App'
 }
 </script>
 
